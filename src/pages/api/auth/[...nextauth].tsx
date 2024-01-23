2ba48bacb7440cbcbb0948991ece27f11a7aa363
Dkk2003 <darshan1010prajapati@gmail.com>
2024-01-23 12:54:00 +0530

    github issue solved

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -3,8 +3,8 @@ import GithubProvider from "next-auth/providers/github";
 export const authOptions = {
   providers: [
     GithubProvider({
-      clientId: "7cf82672c272cdaffae4" || "",
-      clientSecret: "f8c7b5fe7922600dab842e52d3ad5b8434452127" || "",
+      clientId: "7cf82672c272cdaffae4",
+      clientSecret: "9d2bc3245feeb9c1c95f64899c7c309d87054b47",
     }),
   ],
 };
